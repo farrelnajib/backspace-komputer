caee0eed99efcfa1dc3151fa02e159c257dc8c51
Farrel Anshary <farrelnajib@gmail.com>
2020-02-26 20:17:51 +0700

    Update logout modal

--- application/views/admin/_partials/modal.php
+++ application/views/admin/_partials/modal.php
@@ -3,15 +3,14 @@
   <div class="modal-dialog" role="document">
     <div class="modal-content">
       <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalLabel">Ready to Leave?</h5>
+        <h5 class="modal-title" id="exampleModalLabel">Are you sure want to log out?</h5>
         <button class="close" type="button" data-dismiss="modal" aria-label="Close">
           <span aria-hidden="true">×</span>
         </button>
       </div>
-      <div class="modal-body">Select "Logout" below if you are ready to end your current session.</div>
       <div class="modal-footer">
         <button class="btn btn-secondary" type="button" data-dismiss="modal">Cancel</button>
-        <a class="btn btn-primary" href="<?php echo base_url('admin/logout'); ?>">Logout</a>
+        <a class="btn btn-danger" href="<?php echo base_url('admin/logout'); ?>">Logout</a>
       </div>
     </div>
   </div>
